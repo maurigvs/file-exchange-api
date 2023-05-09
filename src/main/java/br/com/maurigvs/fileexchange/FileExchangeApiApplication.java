ae5d85876f9d588478bd12cf0275034567fb1a87
Mauri Gonçalves <mauri.goncalves@accountfy.com>
2023-05-09 11:24:05 -0300

    first commit

---
+++ src/main/java/br/com/maurigvs/fileexchange/FileExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.fileexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExchangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileExchangeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/maurigvs/fileexchange/FileExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.fileexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExchangeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
